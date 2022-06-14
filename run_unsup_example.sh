8a8add31a4512c8aa6c5bbb30bd8c06f96cde7c4
zyznull <zhangyanzhao00@gmail.com>
2022-06-14 08:45:41 +0800

    Update run_unsup_example.sh
    update run_unsup.sh

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-#SBATCH -o single_base_seed.%j.out
-#SBATCH -p inspur
-#SBATCH -w inspur-gpu-01
-
-# In this example, we show how to train SimCSE on unsupervised Wikipedia data.
-# If you want to train it with multiple GPU cards, see "run_sup_example.sh"
-# about how to use PyTorch's distributed data parallel.
 for seed in 42
 do
-MODEL_PATH=result/my-unsup-simcse-z4adapt-mixup-V100-temp0.05-lambda0.5-bert-base-uncased_seed${seed}_test
+MODEL_PATH=result/my-unsup-simcse-z4adapt-mixup-V100-temp0.05-lambda0.2-bert-base-uncased_seed${seed}_test
 
 CUDA_VISIBLE_DEVICES=2 python3 -u train.py \
     --model_name_or_path '/home/LAB/zhangyz/code/pretrained_models/bert-base-uncased' \
@@ -29,7 +21,7 @@ CUDA_VISIBLE_DEVICES=2 python3 -u train.py \
     --do_train \
     --do_eval \
     --seed ${seed} \
-    --lambdas 0.6 \
+    --lambdas 0.2 \
     "$@"
 
 python3 simcse_to_huggingface.py --path=$MODEL_PATH
